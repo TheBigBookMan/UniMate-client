48c9c2b635532542b3c507a41646d8a43890c0b0
Ben Smerd <bsmerd@adam.com.au>
2024-06-30 17:24:27 +0930

    add in dynamic UniversityName in header

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -3,20 +3,22 @@ import { FaMessage } from "react-icons/fa6";
 import { IoSettingsSharp } from "react-icons/io5";
 import { FaBell } from "react-icons/fa";
 import { Link, useLocation } from "react-router-dom";
+import useUserContext from "../../../hooks/useUserContext";
 
 const Header = () => {
     const { pathname } = useLocation();
+    const { user } = useUserContext();
 
     return (
         <div className="border-b-slate-700 border-b p-1 h-[50px] flex justify-between">
-            <div className="flex gap-2 items-center  w-4/6">
-                <Link to="/">
-                    <img src={flinders} className="w-12 rounded-lg" />
-                </Link>
-                <p className="text-slate-300 text-sm">
-                    Royal Melbourne Institute of Technology
-                </p>
-            </div>
+            {user && (
+                <div className="flex gap-2 items-center h-full w-4/6">
+                    <Link to="/">
+                        <img src={flinders} className="w-12 rounded-lg" />
+                    </Link>
+                    <p className="text-slate-300 text-sm">{user.University}</p>
+                </div>
+            )}
             <div className="w-2/6 flex gap-4 pr-2 items-center justify-end">
                 <Link
                     to="/messages"
